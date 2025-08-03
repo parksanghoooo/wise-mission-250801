6f6057bf326948e3e180f1498047f1c9b98986d1
parksanghoooo <tkdgn407@naver.com>
2025-08-03 20:58:01 +0900

    feat: 삭제

--- wiseSaying/src/main/java/com/ll/wisesaying/domain/wiseSaying/repository/WiseSayingRepository.java
+++ wiseSaying/src/main/java/com/ll/wisesaying/domain/wiseSaying/repository/WiseSayingRepository.java
@@ -4,6 +4,8 @@ import com.ll.wisesaying.domain.wiseSaying.model.entity.WiseSaying;
 import com.ll.wisesaying.external.mysql.util.DBUtil;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WiseSayingRepository {
 
@@ -28,5 +30,44 @@ public class WiseSayingRepository {
 
         return -1;
     }
+
+    public List<WiseSaying> findAllDesc() {
+        String sql = "SELECT id, content, author FROM wiseSayings ORDER BY id DESC";
+        List<WiseSaying> list = new ArrayList<>();
+
+        try (
+                Connection conn = DBUtil.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                ResultSet rs = pstmt.executeQuery();
+        ) {
+            while (rs.next()) {
+                long id = rs.getLong("id");
+                String content = rs.getString("content");
+                String author = rs.getString("author");
+
+                list.add(new WiseSaying(id, content, author));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return list;
+    }
+
+    public boolean deleteById(long id) {
+        String sql = "DELETE FROM wiseSayings WHERE id = ?";
+
+        try (
+                Connection conn = DBUtil.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
+            pstmt.setLong(1, id);
+            int result = pstmt.executeUpdate();
+            return result > 0; // 성공 시 1 반환
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
 }
