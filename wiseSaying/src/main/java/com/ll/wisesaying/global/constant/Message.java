96ae36d2d1bb3ca9e179c490a40fc30873d5a491
parksanghoooo <tkdgn407@naver.com>
2025-08-03 20:41:31 +0900

    feat: message constant

---
+++ wiseSaying/src/main/java/com/ll/wisesaying/global/constant/Message.java
@@ -0,0 +1,12 @@
+package com.ll.wisesaying.global.constant;
+
+public class Message {
+
+    public static final String APP_TITLE = "== 명언 앱 ==";
+    public static final String INPUT_PROMPT = "명령) ";
+    public static final String REGISTER_CONTENT = "명언 : ";
+    public static final String REGISTER_AUTHOR = "작가 : ";
+    public static final String REGISTER_SUCCESS = "%d번 명언이 등록되었습니다.\n";
+
+
+}
