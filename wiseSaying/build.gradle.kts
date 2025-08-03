fc37f3f7b156510d5b8181ed941a866164298c58
parksanghoooo <tkdgn407@naver.com>
2025-08-03 20:44:28 +0900

    feat: mysql dependency

--- wiseSaying/build.gradle.kts
+++ wiseSaying/build.gradle.kts
@@ -10,6 +10,13 @@ repositories {
 }
 
 dependencies {
+
+    /**
+     * DB 의존성
+     * - MySQL
+     */
+    implementation("com.mysql:mysql-connector-j:8.3.0")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
