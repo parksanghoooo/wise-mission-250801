63cf8b978b821219f7b97694d77757ce7722ea3d
parksanghoooo <tkdgn407@naver.com>
2025-08-04 20:21:49 +0900

    feat: command 관련 상수

---
+++ wiseSaying/src/main/java/com/ll/wiseSaying/global/constant/Command.java
@@ -0,0 +1,11 @@
+package com.ll.wiseSaying.global.constant;
+
+public class Command {
+
+    public static final String QUIT = "종료";
+    public static final String REGISTER = "등록";
+    public static final String LIST = "목록";
+    public static final String DELETE = "삭제";
+    public static final String UPDATE = "수정";
+
+}
