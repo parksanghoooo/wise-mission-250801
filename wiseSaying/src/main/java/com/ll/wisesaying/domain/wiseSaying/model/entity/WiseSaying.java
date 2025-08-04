97e4e573d73de635371ef8d6375552a9580698ee
parksanghoooo <tkdgn407@naver.com>
2025-08-04 20:57:46 +0900

    feat: wise saying entity

---
+++ wiseSaying/src/main/java/com/ll/wisesaying/domain/wiseSaying/model/entity/WiseSaying.java
@@ -0,0 +1,27 @@
+package com.ll.wisesaying.domain.wiseSaying.model.entity;
+
+public class WiseSaying {
+
+    private long id;
+    private String content;
+    private String author;
+
+    public WiseSaying(long id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+}
